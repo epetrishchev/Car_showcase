ce822add01776b3eed3bef84240f24fc8654ec3b
epetrishchev <epetrishchev@edispa.com>
2023-07-12 01:30:25 +0300

    Сделал карточки машин и получение данных от API

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -6,18 +6,28 @@ import { CustomButtonProps } from "@/types";
 
 
 const CustomButton = ({
-title, containerStyles, handleClick, btnType
+title, containerStyles, handleClick, btnType, textStyles, rightIcon, isDisaled
 }: CustomButtonProps) => {
   return (
     <button
       disabled={false}
-      btnType={btnType}
+      type={btnType}
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
-      <span className={`flex-1`}>
+      <span className={`flex-1 ${textStyles}`}>
         {title}
       </span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image
+            src={rightIcon}
+            alt="right icon"
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
     </button>
   )
 }
